c24f9a5a23b2ea85d5393f1b56b15679cb56e27b
Ale Sanchez <alesanchezmedina@gmail.com>
2021-10-19 19:12:13 +0000

    Added boxplot of times

--- dashboard/app/src/models_evaluation.py
+++ dashboard/app/src/models_evaluation.py
@@ -1,7 +1,9 @@
+import altair as alt
 import streamlit as st
-from streamlit.delta_generator import DeltaGenerator
+from pandas import DataFrame, read_csv
 
 from .classes import Page
+from .config import STREAMLIT_STATIC_PATH
 
 
 class ModelsEvaluation(Page):
@@ -9,4 +11,23 @@ class ModelsEvaluation(Page):
         title = "Models Evaluation"
         st.title(title)
 
-        st.header("Convolutional Neural Network")
+        st.header("Prediction execution time")
+
+        execution_times = read_csv(STREAMLIT_STATIC_PATH / "data/time_results.csv")
+        execution_times: DataFrame
+
+        boxplot = (
+            (
+                alt.Chart(execution_times)
+                .mark_boxplot(size=50, extent=0.5)
+                .encode(
+                    x=alt.X("model:N", scale=alt.Scale(type="log")),
+                    y=alt.Y("time:Q", scale=alt.Scale(zero=False)),
+                    color=alt.Color("model", legend=None),
+                )
+            )
+            .properties(width=900, height=600)
+            .configure_axis(labelFontSize=16, titleFontSize=16)
+        )
+
+        st.altair_chart(boxplot)
